addef30854163479b0329d3fd81cc733dfe35ada
Chandresh Singh <chaharchandresh@gmail.com>
2026-03-26 13:44:17 +0530

    Update author IDs and profile image URLs in `BookAuthor`
    This includes:
    - Updating Open Library IDs (OLIDs) for several authors to ensure accurate data retrieval.
    - Replacing external image links with official Open Library cover API URLs for author profile pictures.
    - Standardizing the image URL format to use the Open Library `olid` parameter with a fallback option.

--- app/src/main/java/com/example/libri/utils/BookAuthor.kt
+++ app/src/main/java/com/example/libri/utils/BookAuthor.kt
@@ -4,67 +4,66 @@ sealed class BookAuthor(val olid: String, val displayName: String, val imageUrl:
 
     // Fantasy & Sci-Fi
     data object J_K_ROWLING : BookAuthor(
-        "OL311W",
+        "OL23919A",
         "J.K. Rowling",
-        "https://images.mubicdn.net/images/cast_member/23749/cache-699739-1627338129/image-w856.jpg"
+        "https://covers.openlibrary.org/a/olid/OL23919A-L.jpg?default=false"
     )
 
     data object J_R_R_TOLKIEN : BookAuthor(
-        "OL123W",
+        "OL26320A",
         "J.R.R. Tolkien",
-        "https://cdn.britannica.com/65/66765-050-63A945A7/JRR-Tolkien.jpg"
+        "https://covers.openlibrary.org/a/olid/OL26320A-L.jpg?default=false"
     )
 
     data object GEORGE_RR_MARTIN : BookAuthor(
-        "OL26242A",
+        "OL234664A",
         "George R.R. Martin",
-        "https://images.gr-assets.com/authors/1351200547p8/346732.jpg"
+        "https://covers.openlibrary.org/a/olid/OL234664A-L.jpg?default=false"
     )
 
     // Thriller & Mystery
-
     data object STEPHEN_KING : BookAuthor(
-        "OL21622A",
+        "OL19981A",
         "Stephen King",
-        "https://authors.b-cdn.net/wp-content/uploads/2020/07/stephen-king.jpg"
+        "https://covers.openlibrary.org/a/olid/OL19981A-L.jpg?default=false"
     )
 
     data object AGATHA_CHRISTIE : BookAuthor(
         "OL27695A",
         "Agatha Christie",
-        "https://images.gr-assets.com/authors/1574341147p8/123715.jpg"
+        "https://covers.openlibrary.org/a/olid/OL27695A-L.jpg?default=false"
     )
 
     data object DAN_BROWN : BookAuthor(
-        "OL26354A",
+        "OL39307A",
         "Dan Brown",
-        "https://images.gr-assets.com/authors/1399399211p8/630.jpg"
+        "https://covers.openlibrary.org/a/olid/OL39307A-L.jpg?default=false"
     )
 
     // Contemporary & Literary
     data object HARUKI_MURAKAMI : BookAuthor(
-        "OL118077A",
+        "OL382524A",
         "Haruki Murakami",
-        "https://images.gr-assets.com/authors/1614760596p8/3354.jpg"
+        "https://covers.openlibrary.org/a/olid/OL382524A-L.jpg?default=false"
     )
 
     data object COLLEEN_HOOVER : BookAuthor(
-        "OL7135010A",
+        "OL7315784A",
         "Colleen Hoover",
-        "https://images.gr-assets.com/authors/1647466540p8/5430144.jpg"
+        "https://covers.openlibrary.org/a/olid/OL7315784A-L.jpg?default=false"
     )
 
     // Classics
     data object JANE_AUSTEN : BookAuthor(
         "OL21594A",
         "Jane Austen",
-        "https://images.gr-assets.com/authors/1470483259p8/1265.jpg"
+        "https://covers.openlibrary.org/a/olid/OL21594A-L.jpg?default=false"
     )
 
     data object MARK_TWAIN : BookAuthor(
         "OL18319A",
         "Mark Twain",
-        "https://images.gr-assets.com/authors/1327366363p8/1244.jpg"
+        "https://covers.openlibrary.org/a/olid/OL18319A-L.jpg?default=false"
     )
 
     companion object {
